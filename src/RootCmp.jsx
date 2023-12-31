a768277cb405ef0ceba1d60cb24a6641e30be019
EdenRize <edenrize@gmail.com>
2023-12-31 16:32:36 +0200

    Add contact details page

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 
 import { store } from './store/store'
 import { Home } from './pages/Home.jsx'
+import { ContactDetails } from './pages/ContactDetails.jsx'
 
 
 export function App() {
@@ -16,6 +17,7 @@ export function App() {
           <main className="main-layout full app">
             <Routes>
               <Route element={<Home />} path="/" />
+              <Route element={<ContactDetails />} path="/contact/:contactId" />
             </Routes>
           </main>
           {/* <AppFooter /> */}
---
+++ src/pages/ContactDetails.jsx
@@ -0,0 +1,32 @@
+import { useNavigate, useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { loadContact } from "../store/actions/contact.actions"
+
+export function ContactDetails() {
+    const [contact, setContact] = useState(null)
+    const { contactId } = useParams()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        _loadContact()
+    }, [contactId])
+
+    async function _loadContact() {
+        try {
+            const contact = await loadContact(contactId)
+            setContact(contact)
+        } catch (error) {
+            console.log('Had issues in toy details', error)
+        }
+    }
+
+    if (!contact) return <div>Loading...</div>
+    return (
+        <section className="contact-details">
+            <h1>Contact Details</h1>
+
+            <h2>Name: {contact.name}</h2>
+            <h2>Tel: {contact.tel}</h2>
+        </section>
+    )
+}
