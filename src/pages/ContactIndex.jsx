f20a6092de0dcbd639d18268db85b02298a98e3d
EdenRize <edenrize@gmail.com>
2023-12-31 19:08:53 +0200

    Add nav to edit in index

--- src/pages/ContactIndex.jsx
+++ src/pages/ContactIndex.jsx
@@ -1,12 +1,8 @@
 import { useSelector } from "react-redux"
 import { useEffect, useRef } from "react"
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
-// import { ContactList } from "../cmps/ContactList"
-// import { ContactFilter } from "../cmps/ContactFilter"
-// import { ContactSort } from "../cmps/ContactSort"
 import { loadContacts, removeContact, setFilterBy, setSortBy } from "../store/actions/contact.actions"
-// import { showSuccessMsg, showErrorMsg } from '../services/event-bus.service'
 import { utilService } from "../services/util.service"
 import { ContactList } from "../cmps/ContactList"
 import { ContactFilter } from "../cmps/ContactFilter"
@@ -15,8 +11,7 @@ export function ContactIndex() {
 
     const contacts = useSelector(storeState => storeState.contactModule.contacts)
     const filterBy = useSelector((storeState) => storeState.contactModule.filterBy)
-    // const filterBy = useSelector(storeState => storeState.contactModule.filterBy)
-    // const sortBy = useSelector(storeState => storeState.contactModule.sortBy)
+
 
 
     useEffect(() => {
@@ -30,14 +25,12 @@ export function ContactIndex() {
 
         fetchData()
     }, [filterBy])
-    // }, [filterBy, sortBy])
 
     async function _loadContacts() {
         try {
             await loadContacts()
         } catch (error) {
             console.error('Error loading contacts:', error)
-            // showErrorMsg('Cannot show contacts')
         }
     }
 
@@ -46,22 +39,14 @@ export function ContactIndex() {
     async function onRemoveContact(contactId) {
         try {
             await removeContact(contactId)
-            // showSuccessMsg('Contact removed')
         } catch (err) {
             console.log('Cannot remove contact', err)
-            // showErrorMsg('Cannot remove contact')
         }
     }
 
     function onSetFilter(filterBy) {
         setFilterBy(filterBy)
     }
-
-    // function onSetSortBy(sortBy) {
-    //     setSortBy(sortBy)
-    // }
-
-    // const { name, tel } = filterBy
 
 
     return (
@@ -70,6 +55,8 @@ export function ContactIndex() {
                 filterBy={filterBy}
                 onSetFilterBy={onSetFilter}
             />
+
+            <Link to='/contact/edit'><button>Add Contact</button></Link>
 
             <ContactList contacts={contacts} onRemoveContact={onRemoveContact} />
         </section>
