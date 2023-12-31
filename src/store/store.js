55919a39c195eba082347e49c3afc5025d6b129a
Noam Saar <noamsaar11@gmail.com>
2023-12-31 16:28:25 +0200

    Add UserMsg hangling

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { contactReducer } from "./reducers/contact.reducer.js"
+import { appReducer } from './reducers/app.reducer.js'
 
 import { combineReducers, compose, legacy_createStore as createStore } from "redux"
 
 
 const rootReducer = combineReducers({
     contactModule: contactReducer,
+    appModule: appReducer,
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
